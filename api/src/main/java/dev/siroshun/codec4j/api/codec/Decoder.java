0827549f60331a77608df311121372637e1dc509
Siroshun09 <siro.shun8@gmail.com>
2025-04-28 06:12:48 +0900

    feat(api): add more methods to Encoder and Decoder

--- api/src/main/java/dev/siroshun/codec4j/api/codec/Decoder.java
+++ api/src/main/java/dev/siroshun/codec4j/api/codec/Decoder.java
@@ -5,6 +5,9 @@ import dev.siroshun.codec4j.api.io.In;
 import dev.siroshun.jfun.result.Result;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+import java.util.function.Function;
+
 /**
  * An interface for decoding data from an {@link In}.
  *
@@ -20,4 +23,49 @@ public interface Decoder<T> {
      */
     @NotNull Result<T, DecodeError> decode(@NotNull In in);
 
+    /**
+     * Maps the decoded data to another type.
+     *
+     * @param mapper the {@link Function} to map the decoded data to another type
+     * @param <A>    the type of the mapped data
+     * @return a new {@link Decoder} that maps the decoded data to another type
+     */
+    default <A> @NotNull Decoder<A> map(@NotNull Function<? super T, ? extends A> mapper) {
+        Objects.requireNonNull(mapper);
+        return in -> Decoder.this.decode(in).map(mapper);
+    }
+
+    /**
+     * Maps the decoded data to another type and flattens the {@link Result}.
+     *
+     * @param mapper the {@link Function} to map the decoded data to another type and returns a {@link Result} of the mapping
+     * @param <A>    the type of the mapped data
+     * @return a new {@link Decoder} that maps the decoded data to another type and flattens the {@link Result}
+     */
+    default <A> @NotNull Decoder<A> flatMap(@NotNull Function<? super T, Result<A, DecodeError>> mapper) {
+        Objects.requireNonNull(mapper);
+        return in -> Decoder.this.decode(in).flatMap(mapper);
+    }
+
+    /**
+     * Inspects the decoded data.
+     *
+     * @param inspector the {@link Function} to inspect the decoded data and returns a {@link Result} of the inspection
+     * @return a new {@link Decoder} that inspects the decoded data
+     */
+    default @NotNull Decoder<T> inspect(@NotNull Function<? super T, Result<T, DecodeError>> inspector) {
+        Objects.requireNonNull(inspector);
+        return in -> Decoder.this.decode(in).flatMap(inspector);
+    }
+
+    /**
+     * Catches a {@link DecodeError} and returns a new {@link Result}.
+     *
+     * @param onError the {@link Function} to catch the {@link DecodeError} and returns a {@link Result} of the new {@link Result}
+     * @return a new {@link Decoder} that catches a {@link DecodeError} and returns a new {@link Result}
+     */
+    default @NotNull Decoder<T> catchError(@NotNull Function<? super DecodeError, Result<T, DecodeError>> onError) {
+        Objects.requireNonNull(onError);
+        return in -> Decoder.this.decode(in).flatMapError(onError);
+    }
 }
--- api/src/main/java/dev/siroshun/codec4j/api/codec/Encoder.java
+++ api/src/main/java/dev/siroshun/codec4j/api/codec/Encoder.java
@@ -6,6 +6,9 @@ import dev.siroshun.jfun.result.Result;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.UnknownNullability;
 
+import java.util.Objects;
+import java.util.function.Function;
+
 /**
  * An interface for encoding data to an {@link Out}.
  *
@@ -23,4 +26,37 @@ public interface Encoder<T> {
      */
     <O> @NotNull Result<O, EncodeError> encode(@NotNull Out<O> out, @UnknownNullability T input);
 
+    /**
+     * Maps the input data to another type.
+     *
+     * @param mapper the {@link Function} to map the input data to another type
+     * @param <A>    the type of the mapped data
+     * @return a new {@link Encoder} that maps the input data to another type
+     */
+    default <A> @NotNull Encoder<A> comap(@NotNull Function<? super A, ? extends T> mapper) {
+        Objects.requireNonNull(mapper);
+        return new Encoder<>() {
+            @Override
+            public @NotNull <O> Result<O, EncodeError> encode(@NotNull Out<O> out, @UnknownNullability A input) {
+                return Encoder.this.encode(out, mapper.apply(input));
+            }
+        };
+    }
+
+    /**
+     * Maps the input data to another type and flattens the {@link Result}.
+     *
+     * @param mapper the {@link Function} to map the input data to another type and returns a {@link Result} of the mapping
+     * @param <A>    the type of the mapped data
+     * @return a new {@link Encoder} that maps the input data to another type and flattens the {@link Result}
+     */
+    default <A> @NotNull Encoder<A> flatComap(@NotNull Function<? super A, ? extends Result<? extends T, EncodeError>> mapper) {
+        Objects.requireNonNull(mapper);
+        return new Encoder<>() {
+            @Override
+            public @NotNull <O> Result<O, EncodeError> encode(@NotNull Out<O> out, @UnknownNullability A input) {
+                return mapper.apply(input).flatMap(t -> Encoder.this.encode(out, t));
+            }
+        };
+    }
 }
