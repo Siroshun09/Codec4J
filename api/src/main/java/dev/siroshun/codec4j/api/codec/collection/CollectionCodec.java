12681a5a8ae47becd38f08cfda46de9fd43a59bb
Siroshun09 <siro.shun8@gmail.com>
2025-04-29 13:17:57 +0900

    refactor(api): commonize ListProcessor/SetProcessor to CollectionCodec.Processor

--- api/src/main/java/dev/siroshun/codec4j/api/codec/collection/CollectionCodec.java
+++ api/src/main/java/dev/siroshun/codec4j/api/codec/collection/CollectionCodec.java
@@ -9,8 +9,16 @@ import dev.siroshun.jfun.result.Result;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.UnknownNullability;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 /**
  * A {@link Codec} for collections such as {@link List} and {@link Set}.
@@ -28,7 +36,15 @@ public final class CollectionCodec<E, C> implements Codec<C> {
      * @return a {@link CollectionCodec}
      */
     public static <E> @NotNull CollectionCodec<E, List<E>> list(@NotNull Codec<E> elementCodec) {
-        return create(new ElementProcessors.ListProcessor<>(elementCodec));
+        return create(new Processor<>(
+                elementCodec,
+                ArrayList::new,
+                (list, element) -> {
+                    list.add(element);
+                    return Result.success();
+                },
+                UnaryOperator.identity()
+        ));
     }
 
     /**
@@ -39,7 +55,17 @@ public final class CollectionCodec<E, C> implements Codec<C> {
      * @return a {@link CollectionCodec}
      */
     public static <E> @NotNull CollectionCodec<E, Set<E>> set(@NotNull Codec<E> elementCodec) {
-        return create(new ElementProcessors.SetProcessor<>(elementCodec));
+        return create(new Processor<>(
+                elementCodec,
+                HashSet::new,
+                (set, element) -> {
+                    if (!set.add(element)) {
+                        return new DuplicatedElementError(element).asFailure();
+                    }
+                    return Result.success();
+                },
+                UnaryOperator.identity()
+        ));
     }
 
     /**
@@ -78,5 +104,40 @@ public final class CollectionCodec<E, C> implements Codec<C> {
      * @param element the duplicated element
      */
     public record DuplicatedElementError(Object element) implements DecodeError.Failure {
+    }
+
+    private record Processor<E, C extends Collection<E>>(@NotNull Codec<E> elementCodec, @NotNull Supplier<C> factory,
+                                                         @NotNull BiFunction<C, E, Result<Void, DecodeError>> acceptor,
+                                                         @NotNull UnaryOperator<C> finalizer) implements ElementProcessor<E, C> {
+
+        @Override
+        public @NotNull <O> Result<O, EncodeError> encodeElement(@NotNull Out<O> out, @UnknownNullability E element) {
+            return this.elementCodec.encode(out, element);
+        }
+
+        @Override
+        public @NotNull Iterator<E> toIterator(@UnknownNullability C input) {
+            return Objects.requireNonNull(input).iterator();
+        }
+
+        @Override
+        public @NotNull Result<E, DecodeError> decodeElement(@NotNull In in) {
+            return this.elementCodec.decode(in);
+        }
+
+        @Override
+        public @NotNull C createIdentity() {
+            return this.factory.get();
+        }
+
+        @Override
+        public @NotNull Result<Void, DecodeError> acceptElement(@NotNull C identity, @UnknownNullability E element) {
+            return this.acceptor.apply(identity, element);
+        }
+
+        @Override
+        public @NotNull C finalizeIdentity(@NotNull C identity) {
+            return this.finalizer.apply(identity);
+        }
     }
 }
