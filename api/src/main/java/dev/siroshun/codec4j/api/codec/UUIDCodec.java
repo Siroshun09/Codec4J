624a209132b55e0bf0924cb4d450b404b6fdf34d
Siroshun09 <siro.shun8@gmail.com>
2025-05-05 16:33:22 +0900

    refactor(api): remove static methods from UUIDCodec

--- api/src/main/java/dev/siroshun/codec4j/api/codec/UUIDCodec.java
+++ api/src/main/java/dev/siroshun/codec4j/api/codec/UUIDCodec.java
@@ -1,8 +1,6 @@
 package dev.siroshun.codec4j.api.codec;
 
 import dev.siroshun.codec4j.api.error.DecodeError;
-import dev.siroshun.codec4j.api.error.EncodeError;
-import dev.siroshun.codec4j.api.io.In;
 import dev.siroshun.codec4j.api.io.Out;
 import dev.siroshun.jfun.result.Result;
 import org.jetbrains.annotations.NotNullByDefault;
@@ -22,24 +20,16 @@ public final class UUIDCodec {
      * <p>
      * If the input string is not a valid {@link UUID}, this {@link Codec} will return a {@link InvalidUUIDFormatError} as a {@link DecodeError}.
      */
-    public static final Codec<UUID> UUID_AS_STRING = Codec.codec(UUIDCodec::encodeAsString, UUIDCodec::decodeFromString);
-
-    private static <O> Result<O, EncodeError> encodeAsString(Out<O> out, UUID input) {
-        return out.writeString(input.toString());
-    }
-
-    private static Result<UUID, DecodeError> decodeFromString(In in) {
-        Result<String, DecodeError> result = in.readAsString();
-        if (result.isFailure()) {
-            return result.asFailure();
+    public static final Codec<UUID> UUID_AS_STRING = Codec.STRING.flatXmap(
+        uuid -> Result.success(uuid.toString()),
+        str -> {
+            try {
+                return Result.success(UUID.fromString(str));
+            } catch (IllegalArgumentException e) {
+                return new InvalidUUIDFormatError(str).asFailure();
+            }
         }
-
-        try {
-            return Result.success(UUID.fromString(result.unwrap()));
-        } catch (IllegalArgumentException e) {
-            return new InvalidUUIDFormatError(result.unwrap()).asFailure();
-        }
-    }
+    );
 
     /**
      * A {@link DecodeError.Failure} for {@link UUIDCodec#UUID_AS_STRING}.
